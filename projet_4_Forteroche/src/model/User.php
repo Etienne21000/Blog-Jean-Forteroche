5759f01dfce76369c414436cbd01547e4c3ee982
Etienne Juffard <etiennejuffard@gmail.com>
2019-11-07 10:32:59 +0100

    user entity and manager

---
+++ projet_4_Forteroche/src/model/User.php
@@ -0,0 +1,82 @@
+<?php
+namespace model;
+// require_once 'model/Entity.php';
+
+class User extends Entity
+{
+    //Object's attributes
+    private $id;
+    private $pseudo;
+    private $pass;
+    private $mail;
+    private $user_date;
+
+    public function __construct(array $data)
+    {
+        $this->hydrate = $this->hydrate($data);
+    }
+
+    /*---------------------------------------
+    User setters
+    ----------------------------------------*/
+
+    public function setId($id)
+    {
+        $this->id = (int)$id;
+    }
+
+    public function setPseudo($pseudo)
+    {
+        if (is_string($pseudo))
+        {
+            $this->pseudo = $pseudo;
+        }
+    }
+
+    public function setPassword($pass)
+    {
+        $this->pass = $pass;
+    }
+
+    public function setMail($mail)
+    {
+        $this->mail = $mail;
+    }
+
+    public function setInscriptiondate($user_date)
+    {
+        if(is_string($user_date))
+        {
+            $this->user_date = $user_date;
+        }
+    }
+
+    /*---------------------------------------
+    User Getters
+    ----------------------------------------*/
+
+    public function id()
+    {
+        return $this->id;
+    }
+
+    public function pseudo()
+    {
+        return $this->pseudo;
+    }
+
+    public function passWord()
+    {
+        return $this->pass;
+    }
+
+    public function mail()
+    {
+        return $this->mail;
+    }
+
+    public function user_date()
+    {
+        return $this->user_date;
+    }
+}
---
+++ projet_4_Forteroche/src/model/UserManager.php
@@ -0,0 +1,90 @@
+<?php
+namespace model;
+
+class UserManager extends Manager
+{
+    public function __construct()
+    {
+        $this->db = $this->dbConnect();
+    }
+
+    //Get 3 last users
+    public function getUsers()
+    {
+        $Users = [];
+        $req = $this->db->query('SELECT id, pseudo, mail, DATE_FORMAT(user_date, \'%d/%m/%Y à %Hh%i\')
+        AS user_date FROM users ORDER BY user_date DESC LIMIT 0, 3');
+
+        while ($data = $req->fetch(\PDO::FETCH_ASSOC))
+        {
+            $user = new User($data);
+            $Users[] = $user;
+        }
+        return $Users;
+    }
+
+    //Check if pseudo exist to connect user
+    public function getPseudo($pseudo)
+    {
+        $req = $this->db->prepare('SELECT id, pseudo, pass FROM users WHERE pseudo = ?');
+        $req->execute([
+            $pseudo
+        ]);
+
+        $data = $req->fetch(\PDO::FETCH_ASSOC);
+
+        if(!$data)
+        {
+            return false;
+        }
+        else
+        {
+            return new User($data);
+        }
+    }
+
+    //Verify if new pseudo is availlable
+    public function verifPseudo($pseudo)
+    {
+        $req = $this->db->prepare('SELECT pseudo FROM users WHERE LOWER(pseudo) = ?');
+        $req->execute([
+            strtolower($pseudo)
+        ]);
+
+        return $req->fetch(\PDO::FETCH_ASSOC);
+
+        // return $user;
+    }
+
+    //Check if email exist already
+    public function verifMail($mail)
+    {
+        $req = $this->db->prepare('SELECT mail FROM users WHERE mail = ?');
+        $req->execute([
+            $mail
+        ]);
+
+        return $req->fetch(\PDO::FETCH_ASSOC);
+    }
+
+    //add user in db
+    public function add($pseudo, $mail, $pass)
+    {
+        $req = $this->db->prepare('INSERT INTO users(pseudo, mail, pass, user_date)
+        VALUES(?, ?, ?, NOW())');
+        $req->execute([
+            $pseudo,
+            $mail,
+            $pass
+        ]);
+    }
+
+    //Count users
+    public function countUsers()
+    {
+        $countUsers = $this->db->query('SELECT COUNT(*) FROM users')->fetchColumn();
+
+        return $countUsers;
+    }
+
+}
